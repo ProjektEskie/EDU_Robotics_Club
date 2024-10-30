2eb26bc0f80e69aac6c0a554d02ff54f8a8feb1a
Mark <mark.ma@cytiva.com>
2024-10-30 09:58:17 -0700

    Updated exe build script

---
+++ Codes/Python/Remte_Control_Client/build_exe.bat
@@ -0,0 +1,5 @@
+SET var=%cd%
+cd var
+rmdir /s /q .\build
+rmdir /s /q .\dist
+nicegui-pack --onefile --windowed --name "RCC" main.py
--- Codes/Python/Remte_Control_Client/build_exe.sh
+++
@@ -1,3 +0,0 @@
-rm -r ./build/
-rm -r ./dist/
-nicegui-pack --onefile --windowed --name "RCC" main.py
