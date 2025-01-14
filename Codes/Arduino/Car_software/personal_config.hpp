bcb2f4dd528c3e0c364e5dc9fe04f65775b4ef11
Mark <mark.ma@cytiva.com>
2025-01-14 13:47:39 -0800

    Update BLE car name and platform configuration in personal_config.hpp

--- Codes/Arduino/Car_software/personal_config.hpp
+++ Codes/Arduino/Car_software/personal_config.hpp
@@ -1,7 +1,7 @@
 #ifndef PERSONAL_CONFIG_HPP
 #define PERSONAL_CONFIG_HPP
 
-#define BLE_CAR_NAME "MM_Car"
-#define IS_EGLOO_PLATFORM 1 // Freenove if 0
+#define BLE_CAR_NAME "RClub_Car"
+#define IS_EGLOO_PLATFORM 0 // Freenove if 0
 
 #endif // PERSONAL_CONFIG_HPP
