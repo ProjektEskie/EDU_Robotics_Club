ca392e2e0ad0a687d8c3d760bba076d6aee2d5ca
Mark <mark.ma@cytiva.com>
2025-01-26 07:52:13 -0800

    Add echo sensor initialization and range measurement functionality

--- Codes/Arduino/Car_software/car.cpp
+++ Codes/Arduino/Car_software/car.cpp
@@ -16,6 +16,11 @@ void CAR_init()
   pinMode(LEFT_SPEED_PIN, OUTPUT);
   pinMode(RIGHT_SPEED_PIN, OUTPUT);
 
+  //Echo sensor
+  pinMode(ECHO_TRIGGER_PIN, OUTPUT);
+  pinMode(ECHO_RESULT_PIN, INPUT);
+  digitalWrite(ECHO_TRIGGER_PIN, LOW);
+
   #if IS_EGLOO_PLATFORM
   pinMode(LEFT_DIRECTION_PIN_IN2, OUTPUT);
   pinMode(RIGHT_DIRECTION_PIN_IN4, OUTPUT);
@@ -116,6 +121,21 @@ int CAR_echo_range_cm()
 {
   int range = -1;
 
+  digitalWrite(ECHO_TRIGGER_PIN, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(ECHO_TRIGGER_PIN, HIGH);
+  delayMicroseconds(12);
+  digitalWrite(ECHO_TRIGGER_PIN, LOW);
+
+  int pulse_duration = pulseIn(ECHO_RESULT_PIN, HIGH, ECHO_TIMEOUT_US);
+  if (pulse_duration != 0)
+  {
+    // distance = pulse duration * speed of sound / 2
+    // speed of sound = 343m/s, 34300cm/s, pulse duration is in micro seconds
+    // distance in cm = pulse duration / 1000000 * 34300 / 2
+    // 1/(34300/2/1000000) ~= 58.3
+    range = pulse_duration / 58.0;
+  }
   return range;
 }
 
