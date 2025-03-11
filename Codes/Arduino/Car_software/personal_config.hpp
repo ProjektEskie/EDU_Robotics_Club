d3266ce8e1bb878ed79637bca51ff8da7901ef2a
Mark <mark.ma@cytiva.com>
2025-03-11 12:23:31 -0700

    Removed un-used code

--- Codes/Arduino/Car_software/personal_config.hpp
+++ Codes/Arduino/Car_software/personal_config.hpp
@@ -5,14 +5,4 @@
 #define IS_EGLOO_PLATFORM 0// Freenove if 0
 #define ENABLE_IMU 1
 
-// typedef struct _configuration
-// {
-//     uint8_t EEPROM_start_byte = 0b10101010;
-//     uint8_t is_elegoo_platform = 0;
-//     uint8_t enable_imu = 1;
-//     char ble_car_name[20] = 'RClub_Car';
-// } configuration;
-
-// configuration personal_config;
-
 #endif // PERSONAL_CONFIG_HPP
